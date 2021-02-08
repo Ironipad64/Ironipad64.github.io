f6750baf66540a39f42e18cd74519ce156e7e796
Ironipad64 <Ironipad64@gmail.com>
2021-02-08 13:36:00 -0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 import React from 'react';
 
+const todos = [
+  {
+    name: 'Wash the car',
+    id: 123,
+    finished: false
+  },
+  {
+    name: 'Take out the trash',
+    id: 124,
+    finished: false
+  }
+];
+
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor() {
+    super();
+    this.state = {
+      todos: todos
+    }
+  }
+
+
+
   render() {
     return (
       <div>
