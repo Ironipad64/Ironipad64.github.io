86876fbcba861d02ff07d60225af85eaba6f46c4
Ironipad64 <Ironipad64@gmail.com>
2021-03-18 16:36:57 -0700

    Updates

---
+++ precache-manifest.e275d9dc5c0c7575983b54d15a12cd39.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f4cc5a2e4d3d08139ea2141fc49c28b",
+    "url": "/React-Todo.git/index.html"
+  },
+  {
+    "revision": "b01d2346fee68b1215fc",
+    "url": "/React-Todo.git/static/js/2.1cd56f14.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/React-Todo.git/static/js/2.1cd56f14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad395b29543cfb121b65",
+    "url": "/React-Todo.git/static/js/main.31516819.chunk.js"
+  },
+  {
+    "revision": "1af10064fdcf11e1b9d9",
+    "url": "/React-Todo.git/static/js/runtime-main.e49b2791.js"
+  }
+]);
